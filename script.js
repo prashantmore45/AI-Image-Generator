f45bc0207cd203a102c00a91a52ff3453b66b5c4
prashantmore45 <prashantmorepm05@gmail.com>
2025-09-13 04:35:38 +0530

    feat: add image generation function with Hugging Face API integration and dynamic dimensions

---
+++ script.js
@@ -0,0 +1,140 @@
+const themeToggle = document.querySelector(".theme-Toggle");
+promptForm = document.querySelector(".prompt-from");
+const promptInput = document.querySelector(".prompt-input");
+const promptBtn = document.querySelector(".prompt-btn");
+const modelSelect = document.getElementById("model-select");
+const countSelect = document.getElementById("count-select");
+const ratioSelect = document.getElementById("ratio-select");
+const gridGallery = document.querySelector(".gallery-grid");
+
+const API_KEY = "";
+
+
+const examplePrompts = [
+    "A futuristic cyberpunk city glowing in neon lights with flying cars in the sky, cinematic view",
+    "A magical forest with glowing mushrooms and a crystal-clear river under a starry night",
+    "A dragon flying over an ancient castle during a fiery sunset, fantasy art style",
+    "A portrait of a young woman painted in Van Gogh’s Starry Night style",
+    "A Japanese garden in watercolor painting style with cherry blossoms falling",
+    "A surreal dreamscape with floating islands and giant clocks melting in the sky, inspired by Salvador Dalí",
+    "A golden retriever puppy playing in a sunflower field, ultra-realistic photo",
+    "A plate of sushi on a wooden table with cinematic lighting, food photography style",
+    "An astronaut standing on Mars looking at Earth in the sky, realistic detail",
+    "A cartoon-style video game landscape with pixel art mountains and retro 8-bit vibes"
+];
+
+//Set theme based on saved preference or system default
+(() => {
+    const saveTheme = localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const isDarkTheme = saveTheme === "dark" ||(!saveTheme && systemPrefersDark);
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+    themeToggle.querySelector("i").classList = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+}) ();
+
+//Switch between light and dark themes
+const toggleTheme = () => {
+    const isDarkTheme = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+    themeToggle.querySelector("i").classList = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+};
+
+//Calculate width/height based on chosen ratio
+const getImageDimensions = (aspectRatio, baseSize = 512) => {
+    const [width, height] = aspectRatio.split("/").map(Number);
+    const scaleFactor = baseSize / Math.sqrt(width * height);
+
+    let calculateWidth = Math.round(width * scaleFactor);
+    let calculateHeight = Math.round(height * scaleFactor);
+
+    //Ensures dimensions are multiple of 16 (AI model requirements)
+    calculateWidth = Math.floor(calculateWidth / 16) * 16;
+    calculateHeight = Math.floor(calculateHeight / 16) * 16;
+
+    return {width: calculateWidth, height: calculateHeight};
+};
+
+// Send requests to Hugging Face API to create images
+const generateImages = async (selectModel, imageCount, aspectRatio, promptText) => {
+    const MODEL_URL = `https://router.huggingface.co/hf-inference/models/${selectModel}`;
+    const {width, height} = getImageDimensions(aspectRatio);
+
+    // Create an array of image generation promises
+    const imagePromises = Array.from({length: imageCount}, async(_, i) => {
+
+        // Send request to the AI model API
+        try {
+            const response = await fetch(MODEL_URL, {
+                headers: {
+                    Authorization: `Bearer ${API_KEY}`,
+                    "Content-Type": "application/json",
+                    "x-use-cache": "false",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    inputs: promptText,
+                    parameters: {width, height},
+                    options: {wait_for_model: true, user_cache: false},
+                }),
+            });
+
+            if (!response.ok) throw new Error((await response.json())?.error);
+
+            const result = await response.blob();
+            console.log(result);
+        } catch (error) {
+        console.log(error);
+        }
+    })
+
+    await Promise.allSettled(imagePromises);
+};
+
+const createImageBoxes = (selectModel, imageCount, aspectRatio, promptText) => {
+    gridGallery.innerHTML = "";
+
+    for (let i = 0; i < imageCount; i++) {
+        gridGallery.innerHTML += 
+        `<div class="img-box loading" id="img-box-${i}" style="aspect-ratio: ${aspectRatio}">
+            <div class="status-container">
+                <div class="spinner"></div>
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <p>Generating...</p>
+            </div>
+            <img src="test.png" class="result-img">
+            <div class="img-layout">
+                <button class="img-download-btn">
+                    <i class="fa-solid fa-download"></i>
+                </button>
+            </div>
+        </div>`;
+    }
+
+    generateImages(selectModel, imageCount, aspectRatio, promptText);
+}
+
+//Handle form submision
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    //Get form values
+    selectModel = modelSelect.value;
+    imageCount = parseInt(countSelect.value) || 1;
+    aspectRatio = ratioSelect.value || "1/1";
+    const promptText = promptInput.value.trim();
+
+    createImageBoxes(selectModel, imageCount, aspectRatio, promptText);
+};
+
+//  Fill prompt input with random example
+promptBtn.addEventListener("click", () => {
+    prompt = examplePrompts[Math.floor(Math.random() * examplePrompts.length)];
+    promptInput.value = prompt;
+    promptInput.focus();
+
+});
+
+promptForm.addEventListener("submit", handleFormSubmit);
+
+themeToggle.addEventListener("click", toggleTheme);
